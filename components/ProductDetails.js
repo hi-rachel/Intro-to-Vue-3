36d35cd19b9d1d825bdb3bef491629954b983229
hi-rachel <rachel.uiux@gmail.com>
2024-05-30 00:37:35 +0900

    Components & Props

---
+++ components/ProductDetails.js
@@ -0,0 +1,13 @@
+app.component("product-details", {
+  props: {
+    details: {
+      type: Array,
+      require: true,
+    },
+  },
+  template:
+    /*html*/
+    `<ul>
+         <li v-for="detail in details">{{ detail }}</li>
+    </ul>`,
+});
